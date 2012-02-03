d9b0382f85f37392536ffbdb1127aaa5a93bdb79
Robert Stevenson <rstevenson542@gmail.com>
2012-02-03 11:55:22 -0600

    Finished layout and routes

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HartlApp::Application.config.secret_token = '7c63eb0c6c4a71a29f4296b31ff8aacde1b6774894fc1ddec488a23f0d873cf8b1c5b514c48e25da98cdf1058184a0bb66c88f656f62b6a776868dd7f779ba95'
